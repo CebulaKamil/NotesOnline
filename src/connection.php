e36f48c80942aca3487eeb4d2df506af399951d6
Cebula Kamil <kontakt@KamilCebula.pl>
2018-06-24 12:10:32 +0200

    Minimized files

--- src/connection.php
+++ src/connection.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("localhost", "root", "", "notes_online");
+$link = mysqli_connect("mysql02.thekamilc1998.beep.pl", "db_notes_admin", "XIRonMen7789X!", "notes_online");
 if(mysqli_connect_error()){
     die('ERROR: Unable to connect:' . mysqli_connect_error()); 
     echo "<script>window.alert('Hi!')</script>";
